437e18d655f467795788cbf3e8df8799294fe4af
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-08-22 00:46:06 +0530

    add multiple reducer

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const createStore = redux.createStore
 // Action
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
    return {
         type : BUY_CAKE,
         info : 'First redux action'
-    }
-    
+    }    
+}
+
+function buyIcecream() {
+   return {
+        type : BUY_ICECREAM,
+        info : 'second redux action'
+    }    
 }
 
 // Reducer
 // (prevState, action) => newState
 
-const initialState = {
+const cakeInitialState = {
     numOfCakes : 10
 }
 
-const reducer = (state = initialState, action) => {
+const icecreamInitialState = {
+    numOfIcecream : 20
+}
+
+const cakeReducer = (state = cakeInitialState, action) => {
     switch(action.type) {
         case BUY_CAKE : return {
             ...state ,
@@ -32,15 +43,46 @@ const reducer = (state = initialState, action) => {
 
     }
 }
+const Icecreamreducer = (state = icecreamInitialState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM : return {
+            ...state ,
+            numOfIcecream : state.numOfIcecream - 1
+        }
+
+        // cases .......
+
+        default : return state 
+
+    }
+}
 
  
 // create store 
 
-const store = createStore(reducer)
+const rootReducer = redux.combineReducers({
+    cake : cakeReducer,
+    icecream : Icecreamreducer
+})
+
+const store = createStore(rootReducer)
 console.log('Initial State - ' , store.getState());
  const unsubscribe = store.subscribe(() => console.log('Updated state - ' , store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
 
-// unsubscribe()
+unsubscribe()
+
+// OutPut
+
+// Initial State -  { cake: { numOfCakes: 10 }, icecream: { numOfIcecream: 20 } }
+// Updated state -  { cake: { numOfCakes: 9 }, icecream: { numOfIcecream: 20 } }
+// Updated state -  { cake: { numOfCakes: 8 }, icecream: { numOfIcecream: 20 } }
+// Updated state -  { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 20 } }
+// Updated state -  { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 19 } }
+// Updated state -  { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 18 } }
+// Updated state -  { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 17 } }
